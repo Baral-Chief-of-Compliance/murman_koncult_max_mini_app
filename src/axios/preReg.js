6671ffb4bf4a2f795f30be807cffea33e5677168
Evgeniy Glushchenko <78692798+Baral-Chief-of-Compliance@users.noreply.github.com>
2026-06-09 16:24:13 +0300

    Update preReg.js

---
+++ src/axios/preReg.js
@@ -0,0 +1,50 @@
+import { apiPreReg } from "src/boot/axiosPreReg";
+
+
+class PrePregistrationService{
+
+    /**
+     * Получить все Электронные очереди для перергистрации
+     */
+    async getAll(){
+        const res = await apiPreReg.get('/')
+
+        return res
+    }
+
+    /**
+     * Проверить находиться ли очередь в онлайн статусе
+     * @param {number} preRegId 
+     * @returns 
+     */
+    async checkOnline(preRegId){
+        const res = await apiPreReg.get(`/${preRegId}/online/`)
+        
+        return res
+    }
+
+
+    /**
+     * Получить свободные даты для записи
+     * @param {number} preRegId 
+     */
+    async getAvailableDates(preRegId){
+        const res = await apiPreReg.get(`/${preRegId}/get_available_dates/`)
+        return res        
+    }
+
+
+    /**
+     * Записаться
+     * @param {number} preRegId 
+     * @param {string} selectDateTime 
+     * @returns 
+     */
+    async makeRecord(preRegId, selectDateTime){
+        const res = await apiPreReg.post(`/${preRegId}/make_record/`)
+        return res
+    }
+}
+
+
+export default PrePregistrationService
